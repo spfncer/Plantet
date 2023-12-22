df521591c552a7f7d7b852301be01f18bec469a0
sfasulo <spencerf0123@gmail.com>
2023-12-21 20:56:06 -0500

    View grid with navpath and pages

--- Plantet/ContentView.swift
+++ Plantet/ContentView.swift
@@ -11,30 +11,45 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var plants: [Plant]
-    
+    @State private var navpath: [Plant] = []
     let grids = [GridItem(.adaptive(minimum: 175))]
+    
+    func goHome(){
+        navpath = .init()
+    }
+    
+    func remove(_ item: Plant){
+        modelContext.delete(item)
+    }
+    
     var body: some View {
         VStack{
-            NavigationView {
+            NavigationStack(path: $navpath) {
                 ScrollView(.vertical){
                     LazyVGrid(columns: grids){
                         ForEach(plants, id: \.self){ plant in
-                            NavigationLink(destination: PlantPage(item: plant.name)) {
-                                PlantCard(name: plant.name, len: 500)
-                                    .onTapGesture(count: 2) {
-                                        print("Double tapped!")
-                                    }
+                            NavigationLink(value: plant){
+                                PlantCard(name: plant.name, len: 30)
                             }
                         }
                     }
+                    .navigationDestination(for:Plant.self){ plant in
+                        PlantPage(item: plant, all: modelContext, cv: self)
+                    }
                     .navigationTitle("Plantet")
+                    .toolbar{
+                        ToolbarItem(placement: .topBarTrailing){
+                            Button("Edit"){
+                                //code
+                            }
+                        }
+                    }
                 }
             }
             Button("Add Plant"){
                 let newPlant = Plant(datePlanted: Date(), frequency: Frequency.xDays(3), name: "Planty", species: "JJ", light: Lighting.direct, lastPruned: Date(), lastFertilized: Date())
                 modelContext.insert(newPlant)
             }
-            
         }
     }
 }
