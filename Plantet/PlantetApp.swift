e40527e0a14c56a6988ed1293c4a9289640ba167
sfasulo <spencerf0123@gmail.com>
2023-12-23 13:07:50 -0500

    Use init in App

--- Plantet/PlantetApp.swift
+++ Plantet/PlantetApp.swift
@@ -10,19 +10,23 @@ import SwiftData
 
 @main
 struct PlantetApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Plant.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    var sharedModelContainer: ModelContainer
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    init() {
+        sharedModelContainer = {
+            let schema = Schema([
+                Plant.self,
+            ])
+            let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
+            do {
+                return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            } catch {
+                fatalError("Could not create ModelContainer: \(error)")
+            }
+        }()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
